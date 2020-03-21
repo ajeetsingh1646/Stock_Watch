932a11344dc59c8ffe3d0fcc460bfbf88ffd894a
ajeetsingh1646 <ajeetsingh1646@gmail.com>
2020-03-21 22:33:47 +0530

    API used and tested

--- stockQoutes/views.py
+++ stockQoutes/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    if request.method == "POST":
+        quote = request.POST['quotetosearch']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/"+quote+"/quote?token=pk_c8e987d2696e45c4abcfd5f44b8d24c0")
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'home.html', {'api' : api})
+    else:
+        return render(request, 'home.html', {'quote' : "Enter something above in the Search box..."})
 
 
 def about(request):
