b2ea6cfa8ec40646aafe7f886ad94a6674704534
ajeetsingh1646 <ajeetsingh1646@gmail.com>
2020-03-26 19:52:26 +0530

    Final Step

--- stockQoutes/models.py
+++ stockQoutes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+    quote = models.CharField(max_length = 10)
+
+
+    def __str__(self):
+        return self.quote
+
+        
--- stockQoutes/views.py
+++ stockQoutes/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Stock
+from .form import StockForm
+from django.contrib import messages
 
 def home(request):
     import requests
@@ -19,3 +21,40 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def add_stock(request):
+    import requests
+    import json
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock has been saved."))
+            return redirect('add_stock')
+
+    else:
+        quote = Stock.objects.all()
+        output = []
+        for quoteItem in quote:
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/"+str(quoteItem)+"/quote?token=pk_c8e987d2696e45c4abcfd5f44b8d24c0")
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+
+        return render(request, 'add_stock.html', {'quote' : quote,
+        'output' : output,
+        })
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("Stock has been deleted successfully!"))
+    return redirect('deleteStock')
+
+
+def deleteStock(request):
+    quote = Stock.objects.all()
+    return render(request, 'deleteStock.html', {'quote' : quote})
